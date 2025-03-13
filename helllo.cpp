e1ca021b798c771d015f20d633a2e77adaeb8272
MainakDasgupta21 <dasguptamainak37@gmail.com>
2025-03-13 12:29:43 +0530

    hello

--- helllo.cpp
+++ helllo.cpp
@@ -28,3 +28,93 @@ using namespace std;
 int main(){
   return 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  return 0;
+}
--- ooooo.cpp
+++ ooooo.cpp
@@ -28,3 +28,93 @@ using namespace std;
 int main(){
   return 0;
 }
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
